1f693c2f7f54e065ab894b472bf41323751e14b9
Abdulrahman2o2 <Abdulrahman.hussienn@gmail.com>
2023-10-17 09:37:50 -0700

    push new file c primemvroin.c

--- prinenvroin.c
+++ prinenvroin.c
@@ -34,3 +34,17 @@ return (0);
  *
  * Return: the value
  */
+char *_getenv(info_t *info, const char *name)
+{
+list_t *node = info->env;
+char *p;
+
+while (node)
+{
+p = starts_with(node->str, name);
+if (p && *p)
+return (p);
+node = node->next;
+}
+return (NULL);
+}
