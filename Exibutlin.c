69cf8fba817cee08219b5c218270390ffe19b2ce
Abdulrahman2o2 <Abdulrahman.hussienn@gmail.com>
2023-10-17 09:04:42 -0700

    push new file c

--- Exibutlin.c
+++ Exibutlin.c
@@ -15,3 +15,31 @@ if (0)
 _puts(*arg_array); /* temp att_unused workaround */
 return (0);
 }
+/**
+ * _myexit - get out of the shell
+ * @info: Struct contain estimated aurgements. Used to maintaing
+ * const function datatype.
+ * Return: exits with a given status.
+ * (0) if info.argv[0] != "exit"
+ */
+int _myexit(info_t *info)
+{
+int exitcheck;
+
+if (info->argv[1]) /* If there is an exit arguement */
+{
+exitcheck = _erratoi(info->argv[1]);
+if (exitcheck == -1)
+		{
+info->status = 2;
+print_error(info, "Illegal number: ");
+_eputs(info->argv[1]);
+_eputchar('\n');
+return (1);
+		}
+info->err_num = _erratoi(info->argv[1])
+return (-2);
+}
+info->err_num = -1;
+return (-2);
+}
