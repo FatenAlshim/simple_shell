2744770b6982f70e82af3ebfadca903db4aa9a79
FatenAlshim <Faten_alshim@yahoo.com>
2023-10-17 09:58:27 -0700

    add3files

---
+++ simple_ataio.c
@@ -0,0 +1,53 @@
+#include "shell.h"
+/**
+ * _atoi - converts string to integerr
+ * @s: string to be convertedd
+ * Return: 0 if no no.in string, converted no. otherwise
+ */
+
+int _atoi(char *s)
+{
+int i, sign = 1, flag = 0, output;
+unsigned int result = 0;
+for (i = 0; s[i] != '\0' && flag != 2; i++)
+{
+if (s[i] == '-')
+sign *= -1;
+if (s[i] >= '0' && s[i] <= '9')
+{
+flag = 1;
+result *= 10;
+result += (s[i] - '0');
+}
+else if (flag == 1)
+flag = 2;
+}
+if (sign == -1)
+output = -result;
+else
+output = result;
+return (output);
+}
+/**
+ * is_delim - if character is  delimeterr
+ * @c: charr to checkk
+ * @delim: delimeterr stringg
+ * Return: 1 if truee, 0 if falsee
+ */
+int is_delim(char c, char *delim)
+{
+while (*delim)
+if (*delim++ == c)
+return (1);
+return (0);
+}
+/**
+ * interactive - returnss true at shell >> interactive mode
+ * @info: struct addresss
+ *
+ * Return: 1 if interactive modee, 0 otherwisee
+ */
+int interactive(info_t *info)
+{
+return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
---
+++ strinerrors.c
@@ -0,0 +1,60 @@
+#include "shell.h"
+/**
+ * _putfd - writes character c to given
+ * @c: The characterr to printt
+ * @fd: The filedescriptotr to write to
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - printss an input stringg
+ * @str: string to be printed
+ * @fd: filedescriptor to write too
+ *
+ * Return: no. of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
+/**
+ * _eputs - printss input string
+ * @str: string to bee printedd
+ *
+ * Return: Nothingg
+ */
+void _eputs(char *str)
+{
+int i = 0;
+if (!str)
+return;
+while (str[i] != '\0')
+{
+_eputchar(str[i]);
+i++;
+}
+}
