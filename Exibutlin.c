e903a84da3fec3532033e2d64c6502b736e4315f
Abdulrahman2o2 <Abdulrahman.hussienn@gmail.com>
2023-10-17 09:03:16 -0700

    push new file c

---
+++ Exibutlin.c
@@ -0,0 +1,7 @@
+#include "shell.h"
+/**
+ * _myhelp - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: Always 0
+ */
