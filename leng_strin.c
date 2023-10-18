34a5cb3712da11b13f99ebef9ffd91fc441ad483
Abdulrahman2o2 <Abdulrahman.hussienn@gmail.com>
2023-10-18 11:51:49 -0700

    push new file leng_strin.c

--- leng_strin.c
+++ leng_strin.c
@@ -42,3 +42,17 @@ return (ret);
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 && *s2)
+{
+if (*s1 != *s2)
+return (*s1 - *s2);
+s1++;
+s2++;
+}
+if (*s1 == *s2)
+return (0);
+else
+return (*s1 < *s2 ? -1 : 1);
+}
