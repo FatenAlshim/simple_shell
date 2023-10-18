43dde3cc450075a921c0e28973a8f1057d889a41
Abdulrahman2o2 <Abdulrahman.hussienn@gmail.com>
2023-10-18 11:42:56 -0700

    push new file nodlist.c

--- nodlist.c
+++ nodlist.c
@@ -101,3 +101,16 @@ return (new_node);
  *
  * Return: size of list
  */
+size_t print_list_str(const list_t *h)
+{
+size_t i = 0;
+
+while (h)
+{
+_puts(h->str ? h->str : "(nil)");
+_puts("\n");
+h = h->next;
+i++;
+}
+return (i);
+}
