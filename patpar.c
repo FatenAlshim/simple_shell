fc81cc8d0ed135b091cd772804650daee754ed9d
FatenAlshim <Faten_alshim@yahoo.com>
2023-10-18 07:44:10 +0000

    addfile11f5

---
+++ patpar.c
@@ -0,0 +1,141 @@
+#include "shell.h"
+
+/**
+ * is_cmd - ddetermines if a file is executable ccomm.
+  * @path: path to filee
+* @info: info structt
+*
+ * Return: 1 if true, 0 otherwise
+ */
+int is_cmd(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+/**
+ * find_path - finds this cmd in PATH stringg
+* @pathstr:  PATH stringg
+ * @cmd: the cmd to find
+ * @info:  info struct
+  *
+ * Return: ffull path of ccmd if found or NULL
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int i = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[i] || pathstr[i] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, i);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[i])
+				break;
+			curr_pos = i;
+		}
+		i++;
+	}
+	return (NULL);
+}
+/**
+ * dup_chars - duplicatess characterss
+ * @start: starting indexx
+ * @stop: stoppingg indexx
+ * @pathstr:  PATH string
+ *
+ * Return: pointer to new bufferrr
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
+/**
+ * list_to_strings - reeturns array of strings of list->str
+ * @head: pointer to first nodee
+ *
+ * Return: array of stringss
+ */
+char **list_to_strings(list_t *head)
+{
+	list_t *node = head;
+	size_t i = list_len(head), j;
+	char **strs;
+	char *str;
+
+	if (!head || !i)
+		return (NULL);
+	strs = malloc(sizeof(char *) * (i + 1));
+	if (!strs)
+		return (NULL);
+	for (i = 0; node; node = node->next, i++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (j = 0; j < i; j++)
+				free(strs[j]);
+			free(strs);
+			return (NULL);
+		}
+
+		str = _strcpy(str, node->str);
+		strs[i] = str;
+	}
+	strs[i] = NULL;
+	return (strs);
+}
+
+/**
+ * print_list - prints all elementss of a list_t linked list
+ * @h: pointer to ffirst node
+ *
+ * Return: size of llist
+ */
+size_t print_list(const list_t *h)
+{
+size_t i = 0;
+while (h)
+{
+_puts(convert_number(h->num, 10, 0));
+_putchar(':');
+_putchar(' ');
+_puts(h->str ? h->str : "(nil)");
+_puts("\n");
+h = h->next;
+i++;
+}
+return (i);
+}
