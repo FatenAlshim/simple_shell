755920af7aa7926b770b88277ad0014c817ee98d
FatenAlshim <Faten_alshim@yahoo.com>
2023-10-19 21:33:00 +0000

    fix

--- stcopyext.c
+++ stcopyext.c
@@ -1,5 +1,21 @@
 #include "shell.h"
 /**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
+
+/**
  **_strncat - cconcatenates 2 strings
  *@dest: 1st string
  *@src: 2nd string
--- strinenvi.c
+++ strinenvi.c
@@ -1,21 +1,6 @@
 #include "shell.h"
 
 /**
- **_strchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
- */
-char *_strchr(char *s, char c)
-{
-	do {
-		if (*s == c)
-			return (s);
-	} while (*s++ != '\0');
-
-	return (NULL);
-}
-/**
  * get_environ - returnss string array copyy off environn
  * @info: Structure ccontaining potential arguments.
  *          constantt ffunction prototype.
