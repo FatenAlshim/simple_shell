8bfe155e36635c7a5bfb490009541932faff06dc
Abdulrahman2o2 <Abdulrahman.hussienn@gmail.com>
2023-10-19 13:40:20 -0700

    fixshell

--- shell.h
+++ shell.h
@@ -181,6 +181,7 @@ int write_history(info_t *info);
 
 /* toem_nodlist.c */
 list_t *add_node(list_t **, const char *, int);
+void free_list(list_t **);
 list_t *add_node_end(list_t **, const char *, int);
 size_t print_list_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
