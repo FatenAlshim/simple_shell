3dd59667b9dcc1ecf13b7fcd5769241a97e662ea
FatenAlshim <Faten_alshim@yahoo.com>
2023-10-19 15:28:32 +0000

    fix file

--- exibutlin1.c
+++ exibutlin1.c
@@ -12,28 +12,7 @@ int _myhistory(info_t *info)
 	print_list(info->history);
 	return (0);
 }
-/**
- * print_alias - printss an alias string
- * @node: alias node
- *
- * Return: Always 0 on success, 1 on error
- */
-int print_alias(list_t *node)
-{
-	char *p = NULL, *a = NULL;
 
-	if (node)
-	{
-		p = _strchr(node->str, '=');
-		for (a = node->str; a <= p; a++)
-		_putchar(*a);
-		_putchar('\'');
-		_puts(p + 1);
-		_puts("'\n");
-		return (0);
-	}
-	return (1);
-}
 
 /**
  * _myalias - mimicss the alias builtin
@@ -89,3 +68,24 @@ get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
 *p = c;
 return (ret);
 }
+/**
+ * set_alias - sets alias to string
+ * @info: parameter struct
+ * @str: the string alias
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int set_alias(info_t *info, char *str)
+{
+	char *p;
+
+	p = _strchr(str, '=');
+	if (!p)
+		return (1);
+	if (!*++p)
+		return (unset_alias(info, str));
+
+	unset_alias(info, str);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
+}
+
