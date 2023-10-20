02757ab1c166393cd7cda72b339aec7218df62b5
FatenAlshim <Faten_alshim@yahoo.com>
2023-10-20 14:32:21 +0000

    editf1

--- simple_ataio.c
+++ simple_ataio.c
@@ -59,9 +59,8 @@ return (isatty(STDIN_FILENO) && info->readfd <= 2);
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
 }
-
