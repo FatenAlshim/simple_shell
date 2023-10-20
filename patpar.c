5240e41357d1fea0a2e80263f92528f744d9d456
FatenAlshim <Faten_alshim@yahoo.com>
2023-10-20 15:22:27 +0000

    editf7

--- patpar.c
+++ patpar.c
@@ -9,17 +9,15 @@
  */
 int is_cmd(info_t *info, char *path)
 {
-	struct stat st;
-
-	(void)info;
-	if (!path || stat(path, &st))
-		return (0);
-
-	if (st.st_mode & S_IFREG)
-	{
-		return (1);
-	}
-	return (0);
+struct stat st;
+(void)info;
+if (!path || stat(path, &st))
+return (0);
+if (st.st_mode & S_IFREG)
+{
+return (1);
+}
+return (0);
 }
 /**
  * find_path - finds this cmd in PATH stringg
